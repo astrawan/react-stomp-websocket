39a978c042113a7fd806bb815da23b0b49072115
Wayan Astrawan <astrawan@icloud.com>
2017-11-04 16:42:10 +0800

    nope

--- index.d.ts
+++ index.d.ts
@@ -2,11 +2,11 @@
 declare module "react-stomp-websocket" {
 
     export class StompWebSocket {
-        constructor(connectCallback?: () => void, receiptCallback?: () => void, errorCallback?: () => void, closeCallback?: () => void);
+        constructor(connectCallback: () => void, receiptCallback: () => void, errorCallback: () => void, closeCallback: () => void);
         connect(login: string, passcode: string): void;
-        diconnect(disconnectCallback?: () => void): void;
+        diconnect(disconnectCallback: () => void): void;
         send(destinaction: string, headers: Array<any>, body: any): void;
-        subscribe(destination: string, callback?: () => void, headers: Array<any>): string;
+        subscribe(destination: string, callback: () => void, headers: Array<any>): string;
         unsubscribe(id: string, headers: Array<any>): void;
         begin(transaction: any, headers: Array<string>): void;
         commit(transaction: any, headers: Array<string>): void;
